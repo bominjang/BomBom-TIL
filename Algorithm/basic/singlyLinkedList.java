1583c91ac3291f309dba2699092e3abcb084b86a
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-01-11 23:01:36 +0900

    Update singlyLinkedList.java

--- Algorithm/basic/singlyLinkedList.java
+++ Algorithm/basic/singlyLinkedList.java
@@ -32,3 +32,14 @@ class Node{
     System.out.println(n.data);
   }
 }
+
+public class SinglyLinkedList {
+    public static void main(String[] args) {
+    
+    Node head = new Node(1);
+    head.append(2);
+    head.append(3);
+    head.append(4);
+    head.retreive();
+    }
+}
