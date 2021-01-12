338c2e04aa8ae8a0abe81cec4c0138b02d57c0df
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-01-12 22:39:03 +0900

    Update linkedList.java

--- Algorithm/basic/linkedList.java
+++ Algorithm/basic/linkedList.java
@@ -10,5 +10,36 @@ class LinkedList{
     header = new Node();
   }
   
+  void append(int d){
+    Node end = new Node(d);
+    Node n = this;
+    while(n.next != null){
+      n=n.next;
+    }
+    n.next = end;
+  }
+  void delete(int d){
+    Node n = header;
+    while(n.next != null){
+      if(n.next == d){
+        n.next = n.next.next;
+      }
+      else{
+        n=n.next;
+      }
+    }
+  }
+  void retrieve(){
+    Node n = header.next;
+    while(n.next != null){
+      System.out.print(n.data+"->");
+      n= n.next;
+    }
+    System.out.println(n.data);
+  }
+}
+
+
+  
   
 }
