cd1e544d20f98ed786f4355f363fc81d0842f74b
Bomin Jang <44834680+bominjang@users.noreply.github.com>
2021-01-12 22:40:35 +0900

    Update linkedList.java

--- Algorithm/basic/linkedList.java
+++ Algorithm/basic/linkedList.java
@@ -38,8 +38,19 @@ class LinkedList{
     System.out.println(n.data);
   }
 }
-
-
-  
-  
 }
+
+public class LinkedListNode {
+    public static void main(String[] args) {
+    
+    LinkedList ll = new LinkedList();
+      
+    ll.append(1);
+    ll.append(2);
+    ll.append(3);
+    ll.retrieve();
+    ll.delete(1);
+    ll.retrieve();
+    }
+}
+
